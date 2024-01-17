2cd2623439e4d1e91daaab57e18b7043df8b68b6
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2024-01-17 20:56:05 +0000

    formatting changes

--- src/agentic_llm/tools/__init__.py
+++ src/agentic_llm/tools/__init__.py
@@ -1,5 +1,5 @@
 from agentic_llm.tools.base import ToolABC
-from agentic_llm.tools.linux_shell import LinuxShell
 from agentic_llm.tools.duckduckgo import DDGSearch
+from agentic_llm.tools.linux_shell import LinuxShell
 from agentic_llm.tools.python_interpreter import PythonInterpreter
 from agentic_llm.tools.wikipedia_summaries import WikipediaSummary
